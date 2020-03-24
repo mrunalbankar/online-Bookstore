0fe103d2ef6ea1d3e8e271e25c2342e9ae8dc67c
mrunalb <mrunal95955@gmail.com>
2020-03-24 23:54:17 +0530

    added repository functionality

--- BookStore/src/main/java/com/mb/BookStoreApplication.java
+++ BookStore/src/main/java/com/mb/BookStoreApplication.java
@@ -1,19 +1,40 @@
 package com.mb;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mb.model.Book;
+import com.mb.model.BookCategory;
+import com.mb.repository.BookCategoryRepository;
+import com.mb.repository.BookRepository;
+
 @SpringBootApplication
-public class BookStoreApplication implements CommandLineRunner {
+public class BookStoreApplication  {
+
+	/*
+	 * @Autowired BookRepository bookRepository;
+	 * 
+	 * @Autowired
+	 * 
+	 * BookCategoryRepository bookCategoryRepository;
+	 */
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookStoreApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-
-	}
+	/*
+	 * @Override public void run(String... args) throws Exception {
+	 * //System.out.println(bookRepository.findAll());
+	 * 
+	 * for (Book book : bookRepository.findAll()) {
+	 * System.out.println(book.toString()); } for (BookCategory bookc :
+	 * bookCategoryRepository.findAll()) { System.out.println(bookc.toString()); }
+	 * 
+	 * }
+	 */
 
 }
